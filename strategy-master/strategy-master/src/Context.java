f2fb64afc4e8d703f1f52d8bb7f4736d9519ea08
Andavidoc <80916351+Andavidoc@users.noreply.github.com>
2023-04-28 21:48:00 -0500

    Add files via upload

---
+++ strategy-master/strategy-master/src/Context.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+import java.util.HashMap;
+public class Context {
+    private Istrategy estrategia;
+
+    public Context(Istrategy estrategia) {
+        this.estrategia = estrategia;
+    }
+    public void ejecutar(){
+        this.estrategia.mostrarRuta();
+    }
+    public void cambiarEstrategia(TiposDeVehiculos estrategia) {
+        Map<TiposDeVehiculos, Istrategy> estrategias = new HashMap<>();
+        estrategias.put(TiposDeVehiculos.BicicletaStrategy, new BicicletaStrategy());
+        estrategias.put(TiposDeVehiculos.CarroStrategy, new CarroStrategy());
+        estrategias.put(TiposDeVehiculos.TransportePublicoStrategy, new TransportePublicoStrategy());
+
+        this.estrategia = estrategias.get(estrategia);
+    }
+
+}
